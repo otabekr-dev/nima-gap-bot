a53997ab8d4e9b8a798f26748b771182c07535fe
otabek7997 <reyimbayevotabek7@gmail.com>
2025-08-20 19:47:50 +0500

    done'

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 from config import TOKEN
 import handlers
@@ -12,21 +12,6 @@ def main() -> None:
     dispatcher.add_handler(CommandHandler('start', handlers.start))
 
     dispatcher.add_handler(MessageHandler(Filters.text("Buyurtmalarim"), handlers.order))
-    dispatcher.add_handler(MessageHandler(Filters.text("Sozlamalar"), handlers.settings))
-    dispatcher.add_handler(MessageHandler(Filters.text("Telefon raqingizni ozgartish"), handlers.contact))
-    dispatcher.add_handler(MessageHandler(Filters.text("Orqaga"), handlers.back))
-    dispatcher.add_handler(MessageHandler(Filters.text("Tilni o'zgartish"), handlers.set_language))
-
-    dispatcher.add_handler(CallbackQueryHandler(handlers.choose_language, pattern=r'^language:'))
-
-    dispatcher.add_handler(MessageHandler(Filters.text('Sozlamalar'), handlers.settings))
-
-    dispatcher.add_handler(MessageHandler(Filters.text("Telefon raqingizni ozgartish"), handlers.contact))
-    dispatcher.add_handler(MessageHandler(Filters.text("Orqaga"), handlers.back))
-    dispatcher.add_handler(MessageHandler(Filters.text("Tilni o'zgartish"), handlers.set_language))
-
-    dispatcher.add_handler(CallbackQueryHandler(handlers.choose_language, pattern=r'^language:'))
-
 
     updater.start_polling()
     updater.idle()
